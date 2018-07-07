46e8a326f7f0cfeafcbcbbe2d63b3e556ce1fd99
Alexandre Borrego <contact@alxbrg.com>
2018-07-07 23:15:01 +0200

    Simplified Database API & started using EventEmitter

--- test/database.spec.js
+++ test/database.spec.js
@@ -16,8 +16,6 @@ describe('Database', () => {
     db: loki,
   });
 
-  const { models: { Test } } = db;
-
   beforeAll(async () => {
     await db.connect();
   });
@@ -36,21 +34,29 @@ describe('Database', () => {
       type: Database.IN_MEMORY,
     });
 
+    const onConnect = jest.fn();
+    const onDisconnect = jest.fn();
+
+    _db.on(Database.CONNECT, onConnect);
+    _db.on(Database.DISCONNECT, onDisconnect);
+
     test('connects the db and emits a CONNECT event', async () => {
       await _db.connect();
       expect(_db.connected).toBe(true);
+      expect(onConnect).toHaveBeenCalledTimes(1);
     });
 
     test('disconnects the db and emits a DISCONNECT event', async () => {
       await _db.disconnect();
       expect(_db.connected).toBe(false);
+      expect(onDisconnect).toHaveBeenCalledTimes(1);
     });
   });
 
   describe('create', () => {
     test('creates a single object', async () => {
       const mock = { foo: 1 };
-      const res = await Test.create(mock);
+      const res = await db.create('Test', mock);
       const store = collection.find({});
 
       expect(res).toEqual([{ ...mock, id: 1 }]);
@@ -59,7 +65,7 @@ describe('Database', () => {
 
     test('creates multiple objects', async () => {
       const mocks = [{ foo: 1 }, { foo: 2 }];
-      const res = await Test.create(mocks);
+      const res = await db.create('Test', mocks);
       const store = collection.find({});
 
       expect(res).toEqual(mocks.map((mock, i) => ({ ...mock, id: i + 1 })));
@@ -71,7 +77,7 @@ describe('Database', () => {
     test('deletes objects', async () => {
       const mock = { foo: 3 };
       collection.insert(mock);
-      const res = await Test.delete(mock);
+      const res = await db.delete('Test', mock);
       const store = collection.find({});
 
       expect(res).toEqual([{ ...mock, id: expect.any(Number) }]);
@@ -83,7 +89,7 @@ describe('Database', () => {
     test('finds objects', async () => {
       const mock = { foo: 2 };
       collection.insert(mock);
-      const res = await Test.find(mock);
+      const res = await db.find('Test', mock);
 
       expect(res).toEqual([{ ...mock, id: expect.any(Number) }]);
     });
@@ -93,7 +99,7 @@ describe('Database', () => {
     test('$inc', async () => {
       const mocks = [{ foo: 1, bar: 1 }, { foo: 2, bar: 1 }, { foo: 0, bar: 0 }];
       collection.insert(mocks);
-      const res = await Test.update({ bar: 1 }, { $inc: { foo: 10 } });
+      const res = await db.update('Test', { bar: 1 }, { $inc: { foo: 10 } });
 
       expect(res).toEqual([{ foo: 11, bar: 1, id: 1 }, { foo: 12, bar: 1, id: 2 }]);
     });
@@ -101,7 +107,7 @@ describe('Database', () => {
     test('$mul', async () => {
       const mocks = [{ foo: 1, bar: 1 }, { foo: 2, bar: 1 }, { foo: 0, bar: 0 }];
       collection.insert(mocks);
-      const res = await Test.update({ bar: 1 }, { $mul: { foo: 10 } });
+      const res = await db.update('Test', { bar: 1 }, { $mul: { foo: 10 } });
 
       expect(res).toEqual([{ foo: 10, bar: 1, id: 1 }, { foo: 20, bar: 1, id: 2 }]);
     });
@@ -109,7 +115,7 @@ describe('Database', () => {
     test('$set', async () => {
       const mocks = [{ foo: 1, bar: 1 }, { bar: 1 }, { foo: 0, bar: 0 }];
       collection.insert(mocks);
-      const res = await Test.update({ bar: 1 }, { $set: { foo: 10 } });
+      const res = await db.update('Test', { bar: 1 }, { $set: { foo: 10 } });
 
       expect(res).toEqual([{ foo: 10, bar: 1, id: 1 }, { foo: 10, bar: 1, id: 2 }]);
     });
@@ -117,7 +123,7 @@ describe('Database', () => {
     test('$unset', async () => {
       const mocks = [{ foo: 1, bar: 1 }, { bar: 1 }, { foo: 0, bar: 0 }];
       collection.insert(mocks);
-      const res = await Test.update({ bar: 1 }, { $unset: { foo: 1 } });
+      const res = await db.update('Test', { bar: 1 }, { $unset: { foo: 1 } });
 
       expect(res).toEqual([{ foo: null, bar: 1, id: 1 }, { foo: null, bar: 1, id: 2 }]);
     });
@@ -130,7 +136,7 @@ describe('Database', () => {
         { foo: 0, bar: 1 },
       ];
       collection.insert(mocks);
-      const res = await Test.update(condition, { $pull: { foo: 2 } });
+      const res = await db.update('Test', condition, { $pull: { foo: 2 } });
 
       expect(res).toEqual([
         { foo: [ 1 ], bar: 1, id: 1 },
@@ -139,7 +145,7 @@ describe('Database', () => {
       ]);
 
       // $in
-      const res1 = await Test.update(condition, {
+      const res1 = await db.update('Test', condition, {
         $pull: { foo: { $in: [ 1, 3 ] } },
       });
       expect(res1).toEqual([
@@ -152,7 +158,7 @@ describe('Database', () => {
     test('$push', async () => {
       const mocks = [{ foo: [ 1, 2 ], bar: 1 }, { foo: 1, bar: 1 }, { foo: 0, bar: 0 }];
       collection.insert(mocks);
-      const res = await Test.update({ bar: 1 }, { $push: { foo: 3 } });
+      const res = await db.update('Test', { bar: 1 }, { $push: { foo: 3 } });
 
       expect(res).toEqual([
         { foo: [ 1, 2, 3 ], bar: 1, id: 1 },
