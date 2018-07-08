0ec1b53db01a5868d2a5e38f8482b3c7f8646342
Alexandre Borrego <contact@alxbrg.com>
2018-07-08 02:05:16 +0200

    Simplified Database API & started using EventEmitter

--- src/database/index.js
+++ src/database/index.js
@@ -1,29 +1,35 @@
 'use strict';
 
+const { EventEmitter } = require('events');
+
 const {
   InMemoryStore,
 } = require('./adapters');
 
-const Model = require('./model');
+const CONNECT = 'connect';
+const DISCONNECT = 'disconnect';
 
 const IN_MEMORY = 'in-memory';
 const MONGO = 'mongo';
 const MYSQL = 'mysql';
 
-class Database {
+class Database extends EventEmitter {
   /**
    * @param {object} options
    * @param {object} [options.adapter] optional custom adapter
    * @param {object} [options.db] option custom database client
    * @param {array} options.schemas array of schemas
-   * @param {string} options.type database type ('in-memory', 'mongo' or 'mysql')
+   * @param {string} [options.type] database type ('in-memory', 'mongo' or 'mysql' -
+   * defaults to 'in-memory')
    */
   constructor ({
     adapter,
     db,
     schemas,
-    type,
+    type = IN_MEMORY,
   }) {
+    super();
+
     const options = { db, schemas };
 
     // Pick database adapter
@@ -45,40 +51,99 @@ class Database {
         );
     }
 
-    // Build models
-    this.models = {};
-    for (const { name, definition } of schemas)
-      this.models[name] = new Model({
-        adapter: this._adapter,
-        name,
-        schema: definition,
-      });
+    this.options = options;
+    this._schemas = schemas;
+    this.type = type;
 
     this.connected = false;
-    this.options = options;
-    this.type = type;
   }
 
   /**
    * Connects to the database using the options passed to the constructor.
    */
   async connect () {
-    if (!this.connected) {
-      await this._adapter.connect();
-      this.connected = true;
-    }
+    if (this.connected) return;
+
+    await this._adapter.connect();
+    this.connected = true;
+    this.emit(CONNECT);
   }
 
   /**
    * Shuts down the database connection.
    */
   async disconnect () {
-    if (this.connected) {
-      await this._adapter.disconnect();
-      this.connected = false;
-    }
+    if (!this.connected) return;
+
+    await this._adapter.disconnect();
+    this.connected = false;
+    this.emit(DISCONNECT);
+  }
+
+  /**
+   * Performs an insert of an object or objects into the database.
+   *
+   * @param {string} model
+   * @param {(array|object)} objects object(s) to insert
+   * @param {object} [projection] optional fields to return
+   * @param {object} [options]
+   *
+   * @returns {promise} created objects
+   */
+  create (model, objects, projection, options) {
+    return this._adapter.create(model, objects, projection, options);
+  }
+
+  /**
+   * Deletes all objects matching the conditions.
+   *
+   * @param {string} model
+   * @param {object} conditions
+   * @param {object} [projection] optional fields to return
+   * @param {object} [options]
+   *
+   * @returns {promise} created objects
+   */
+  delete (model, conditions, projection, options) {
+    return this._adapter.delete(model, conditions, projection, options);
+  }
+
+  /**
+   * Finds all objects matching the conditions.
+   *
+   * @param {string} model
+   * @param {object} conditions
+   * @param {object} [projection] optional fields to return
+   * @param {object} [options]
+   *
+   * @returns {promise} created objects
+   */
+  find (model, conditions, projection, options) {
+    return this._adapter.find(model, conditions, projection, options);
+  }
+
+  join (options) {
+    return this._adapter.join(this._name, options);
+  }
+
+  /**
+   * Updates all objects matching the conditions.
+   *
+   * @param {string} model
+   * @param {object} conditions
+   * @param {object} updates updates to perform
+   * @param {object} [projection] optional fields to return
+   * @param {object} [options]
+   *
+   * @returns {promise} created objects
+   */
+  update (model, conditions, updates, projection, options) {
+    return this._adapter.update(model, conditions, updates, projection, options);
   }
 }
+
+Database.CONNECT = CONNECT;
+Database.DISCONNECT = DISCONNECT;
 
 Database.IN_MEMORY = IN_MEMORY;
 Database.MONGO = MONGO;
