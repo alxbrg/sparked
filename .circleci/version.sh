3d6eb3809da967790f82830ecff83ac4daebd9fe
Alexandre Borrego <contact@alxbrg.com>
2018-07-10 00:47:19 +0200

    Fixed version checks

--- .circleci/version.sh
+++ .circleci/version.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+# export $PKG_VERSION and $NPM_VERSION vars
 source .bash_env
 
-# exit with an error if NPM and pkg.json versions are identical
-if [ "$NPM_VERSION" = "$PKG_VERSION" ] ; then
-  echo "Package version ($PKG_VERSION) was not incremented."
-  exit 1
-fi
+function fail () {
+  echo "Version $PKG_VERSION has already been published." ; exit 1
+}
+
+function pass () {
+  echo "All good. Ready to publish version $PKG_VERSION." ; exit 0
+}
+
+# fail if npm and package.json versions are identical
+if [ "$NPM_VERSION" = "$PKG_VERSION" ] ; then fail ; fi
 
 # iterate over each version (major, minor, patch)
 IFS='. ' read -r -a npm_v <<< $NPM_VERSION
@@ -14,13 +20,6 @@ IFS='. ' read -r -a pkg_v <<< $PKG_VERSION
 
 for i in `seq 0 2`
   do
-    # exit with an error if any of the pkg.json versions is less than the corresponding
-    # npm version
-    if [ ${pkg_v[$i]} -lt ${npm_v[$i]} ] ; then
-      echo "Package version ($PKG_VERSION) is behind published version ($NPM_VERSION)."
-      exit 1
-    fi
+    if   [ ${pkg_v[$i]} -gt ${npm_v[$i]} ] ; then pass
+    elif [ ${pkg_v[$i]} -lt ${npm_v[$i]} ] ; then fail ; fi
   done
-
-echo "All good. Ready to publish version $PKG_VERSION."
-exit 0
