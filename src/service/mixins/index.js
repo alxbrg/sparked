602f85f5f2c3d1d8b6684605c9d8a6821695361d
Alexandre Borrego <contact@alxbrg.com>
2018-07-17 20:16:07 +0200

    Implemented basic Api mixin

--- src/service/mixins/index.js
+++ src/service/mixins/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
 module.exports = {
+  Api: require('./api'),
   Manager: require('./manager'),
 };
---
+++ test/service/mixins/api.spec.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const { createServer } = require('http');
+
+const { Service } = require('../../../src');
+
+const Api = Service.use(Service.mixins.Api);
+
+describe('Api', () => {
+  const server = createServer(() => {});
+
+  describe('constructor', () => {
+    test('invalid options', () => {
+      /* eslint-disable no-new */
+      expect(() => { new Api(); }).toThrow();
+      expect(() => { new Api({ server: 1 }); }).toThrow();
+      expect(() => { new Api({ server: { listen: 1 } }); }).toThrow();
+      expect(() => { new Api({ server: { listen: () => {}, close: 1 } }); }).toThrow();
+      expect(() => { new Api({ server, host: 1 }); }).toThrow();
+      expect(() => { new Api({ server, port: 'port' }); }).toThrow();
+      /* eslint-enable no-new */
+    });
+
+    test('valid options', () => {
+      const host = 'host';
+      const port = 4000;
+      const api = new Api({ server, host, port });
+
+      expect(api.host).toEqual(host);
+      expect(api.port).toEqual(port);
+    });
+
+    test('defaults', () => {
+      const api = new Api({ server });
+
+      expect(api.host).toEqual('localhost');
+      expect(api.port).toEqual(8080);
+    });
+  });
+
+  test('connect/disconnect', async () => {
+    const api = new Api({ server });
+
+    // Connect
+    const onListening = jest.fn();
+    api.on(Api.SERVER_LISTENING, onListening);
+    await api.connect();
+
+    expect(api.connected).toBe(true);
+    expect(api.listening).toBe(true);
+    expect(onListening).toHaveBeenCalledTimes(1);
+
+    // Disconnect
+    const onClose = jest.fn();
+    api.on(Api.SERVER_CLOSE, onClose);
+    await api.disconnect();
+
+    expect(api.connected).toBe(false);
+    expect(api.listening).toBe(false);
+    expect(onClose).toHaveBeenCalledTimes(1);
+  });
+});
